87fd5f709a6ecf954b568bded477ca5d90c93d98
Rafael Garcia Garcia <Ralph2g@github.com>
2019-03-13 22:27:14 -0600

    Se agregan detalles a la interfaz,links, y pagina nosotros

---
+++ Pagina HTML/js/main.js
@@ -0,0 +1,116 @@
+    //owl carousel
+        
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoWidht:false,
+    
+        responsive:{
+            0:{
+                items:1,
+            },
+            500:{
+                items:2,
+                margin:20,
+            },
+            768:{
+                items:3,
+                margin:20,
+            
+            },
+            992:{
+                items:4,
+                margin:20,
+            }
+            }
+        })
+
+
+$(document).ready(function(){
+            
+            /*Función:Mostrar y ocultar ventana modal*/
+            
+            $('.btn-buscar').click(function(n){
+                $('#buscador-modal').fadeIn();
+            });
+            
+            $('.btn-cerrar').click(function(n){
+                $('#buscador-modal').fadeOut();
+            });
+            
+            /*Función:Megamenú*/
+            
+            $('.mega-menu .dropdown-menu').click(function(n){
+                n.stopPropagation();
+            })
+            
+            /*Funcion 3: INICIAMOS EL SCRIPT TYPED.JS*/
+            // Can also be included with a regular script tag
+            
+            var options = {
+            strings: [, "Contabilidad para Personas Morales^450","Contabilidad para Personas Físicas^450","Contabilidad para Empresas^450"],
+                
+            typeSpeed: 48,
+            smartBackspace:false,
+            loop:true,
+            backSpeed: 33,
+            startDelay:700
+            }
+            
+            var typed = new Typed(".typed", options);
+            
+});
+
+//Funciones JQuery
+        
+    $(document).ready(function(){
+            
+            $('.dropdown-menu').click(function(n){
+                n.stopPropagation();
+            })
+            
+                          });
+                
+            //Funciones JS//
+            
+            //sticky function;
+            window.onscroll = function(){
+                myFunction();
+            }
+            
+            function myFunction(){
+            var ayuwoki = document.getElementById('encabezado');
+            var padding = document.getElementById('mover')
+            var sticky = ayuwoki.offsetTop;
+        
+             if( window.pageYOffset > sticky ){
+                ayuwoki.classList.add("sticky");
+                padding.classList.add("padding")
+                }else{
+                    
+                ayuwoki.classList.remove("sticky");
+                }
+            }
+        
+        //Spy scroll
+            function functionScroll() {
+            var section = document.querySelectorAll(".section"),
+                sections = {}, i = 0;
+
+                Array.prototype.forEach.call(section, function(e) {
+                sections[e.id] = e.offsetTop;
+            });
+
+                for (i in sections) {
+      if (sections[i] <= window.pageYOffset +100) {
+        document.querySelector('.activo').classList.remove('activo');
+        document.querySelector('a[href*=' + i + ']').classList.add('activo');
+        document.querySelector('a[href*=' + i + ']').classList.add('activo');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', functionScroll);
+  window.addEventListener('resize', functionScroll);
+
+            
